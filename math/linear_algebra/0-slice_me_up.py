8e60384fbd6b75dfd8f9db9dea8e8645e2cd9858
root <meekmarcelin@gmail.com>
2023-09-07 02:11:33 -0700

    1 commit

---
+++ math/linear_algebra/0-slice_me_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+This script slices and displays specific parts of an array.
+"""
+
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 = arr[:2]  # Extract the first two numbers of the array
+arr2 = arr[-5:]  # Extract the last five numbers of the array
+arr3 = arr[1:6]  # Extract the 2nd through 6th numbers of the array
+
