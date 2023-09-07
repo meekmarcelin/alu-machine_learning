6a6997563f75af19ad5bed34dad5e6fc6725d9c0
root <meekmarcelin@gmail.com>
2023-09-07 03:09:22 -0700

    0 commit

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
-
-"""
-This script slices and displays specific parts of an array.
-"""
-
-# Define the array
+""" This script slices and displays specific parts of an arra """
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 
-# Slice the array to get the required parts and print the results
 print("The first two numbers of the array are: {}".format(arr[:2]))
 print("The last five numbers of the array are: {}".format(arr[-5:]))
 print("The 2nd through 6th numbers of the array are: {}".format(arr[1:6]))
-
 
