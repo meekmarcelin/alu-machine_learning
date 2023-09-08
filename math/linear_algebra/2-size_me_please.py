bb666441007d66992292530dd0edeacc6154ce19
root <meekmarcelin@gmail.com>
2023-09-08 07:09:15 -0700

    1 commit

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+2-size_me_please.py
+A module for calculating the shape of a matrix.
+"""
 def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
