89d552fbb813b305522810e06bbb7a710aa917ec
root <meekmarcelin@gmail.com>
2023-09-09 10:38:33 -0700

    4 commit

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-dd_arrays(arr1, arr2):
+def add_arrays(arr1, arr2):
+    # Check if the input arrays have the same length
     if len(arr1) != len(arr2):
         return None
 
@@ -11,4 +12,13 @@ dd_arrays(arr1, arr2):
         result.append(arr1[i] + arr2[i])
 
     return result
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4]
+    arr2 = [5, 6, 7, 8]
+    print(add_arrays(arr1, arr2))
+    print(arr1)
+    print(arr2)
+    print(add_arrays(arr1, [1, 2, 3]))
+
 
