b19802c1fd011564b81355c9a9b714e8a0d65e66
root <meekmarcelin@gmail.com>
2023-09-09 10:59:05 -0700

    5 commit

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 def add_matrices2D(mat1, mat2):
-    # Check if the input matrices have the same shape (same number of rows and columns)
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
 
-    # Initialize an empty matrix (list of lists) to store the result
+  
     result = []
 
-    # Iterate through the rows of mat1 and mat2 and add them element-wise
+   
     for i in range(len(mat1)):
         row_result = []
         for j in range(len(mat1[0])):
@@ -16,6 +15,7 @@ def add_matrices2D(mat1, mat2):
 
     return result
 
+
 if __name__ == "__main__":
     mat1 = [[1, 2], [3, 4]]
     mat2 = [[5, 6], [7, 8]]
@@ -23,4 +23,3 @@ if __name__ == "__main__":
     print(mat1)
     print(mat2)
     print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
-
