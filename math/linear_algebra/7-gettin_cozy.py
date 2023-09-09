d9ccd4bce0e983e002a0854bad9f5e05a81c312b
root <meekmarcelin@gmail.com>
2023-09-09 11:41:39 -0700

    7 commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,8 +9,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
             return None
         concatenated_matrix = [mat1_row + mat2_row for mat1_row, mat2_row in zip(mat1, mat2)]
     else:
-        return None
-    
+        return None    
     return concatenated_matrix
 
 
@@ -18,13 +17,10 @@ if __name__ == "__main__":
     mat1 = [[1, 2], [3, 4]]
     mat2 = [[5, 6]]
     mat3 = [[7], [8]]
-    
     mat4 = cat_matrices2D(mat1, mat2)
     mat5 = cat_matrices2D(mat1, mat3, axis=1)
-    
     print(mat4)
     print(mat5)
-    
     mat1[0] = [9, 10]
     mat1[1].append(5)
     print(mat1)
