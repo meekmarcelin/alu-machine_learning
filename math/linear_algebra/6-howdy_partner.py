bfc92ff8565fdb3ec22a5abd0a0a2312fe1ad2ed
root <meekmarcelin@gmail.com>
2023-09-09 11:30:44 -0700

    6 commit

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def cat_arrays(arr1, arr2):
+    concatenated_array = arr1 + arr2
+    return concatenated_array
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4, 5]
+    arr2 = [6, 7, 8]
+    result = cat_arrays(arr1, arr2)
+    print(result)
+    print(arr1)
+    print(arr2)
