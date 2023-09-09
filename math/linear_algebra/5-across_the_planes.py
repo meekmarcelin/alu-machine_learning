ab4a2353c78ae36574da3219b43d0bea28264b85
root <meekmarcelin@gmail.com>
2023-09-09 11:03:59 -0700

    5 commit

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,10 +3,8 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
 
-  
     result = []
 
-   
     for i in range(len(mat1)):
         row_result = []
         for j in range(len(mat1[0])):
