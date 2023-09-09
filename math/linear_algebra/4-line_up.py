18a33e7f201ea0a5af595ae65d490a8cf7fff4b3
root <meekmarcelin@gmail.com>
2023-09-09 10:41:46 -0700

    4 commit

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 def add_arrays(arr1, arr2):
-    # Check if the input arrays have the same length
+   
     if len(arr1) != len(arr2):
         return None
-
-    # Initialize an empty list to store the result
     result = []
 
-    # Iterate through the elements of arr1 and arr2 and add them element-wise
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
 
@@ -20,5 +17,3 @@ if __name__ == "__main__":
     print(arr1)
     print(arr2)
     print(add_arrays(arr1, [1, 2, 3]))
-
-
