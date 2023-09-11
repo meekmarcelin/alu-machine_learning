6f476e486d2179d3bd62889acbfc4cd70aa3ffce
root <meekmarcelin@gmail.com>
2023-09-11 11:26:46 -0700

    12 commit

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Perform addition, subtraction, multiplication, and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """ do the magic of some syntaxy"""
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+
+    return add, sub, mul, div
