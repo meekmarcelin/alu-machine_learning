f2f662839af8c8ead30748fc8de712ddcf8e13d3
root <meekmarcelin@gmail.com>
2023-09-11 11:55:14 -0700

    7 commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python3
+
+""" concatenate two matrices along a specific axis"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenate at zero axis"""
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
-        concatenated_matrix = mat1 + mat2
-    elif axis == 1:
+        return mat1 + mat2
+    if axis == 1:
         if len(mat1) != len(mat2):
             return None
-        concatenated_matrix = [mat1_row + mat2_row for mat1_row, mat2_row in zip(mat1, mat2)]
-    else:
-        return None    
-    return concatenated_matrix
-
-
-if __name__ == "__main__":
-    mat1 = [[1, 2], [3, 4]]
-    mat2 = [[5, 6]]
-    mat3 = [[7], [8]]
-    mat4 = cat_matrices2D(mat1, mat2)
-    mat5 = cat_matrices2D(mat1, mat3, axis=1)
-    print(mat4)
-    print(mat5)
-    mat1[0] = [9, 10]
-    mat1[1].append(5)
-    print(mat1)
-    print(mat4)
-    print(mat5)
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    return None
