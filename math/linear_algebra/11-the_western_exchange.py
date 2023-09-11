da260fddaeb23d4925b9bf1eccab939c39aa54a5
root <meekmarcelin@gmail.com>
2023-09-11 11:19:24 -0700

    11 commit

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Function that transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """Transpose  matrix"""
+    import numpy as np
+    transposed = np.transpose(matrix)
+    return transposed
