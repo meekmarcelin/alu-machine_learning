d41fe885cf41d495c18541bab3523ab7092e1961
root <root@meekpc.localdomain>
2023-09-22 22:23:22 +0200

    /

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-def summation_i_squared(n):
+""" summation for the given equition """
 
+
+def summation_i_squared(n):
     if not isinstance(n, int) or n <= 0:
         return None
     if n == 1:
         return 1
-
-    
     return n**2 + summation_i_squared(n - 1)
