2f05dd243df5ff57308cee97c961ad81dadbea2b
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-09-30 15:43:26 +0200

    1 commit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -7,27 +7,25 @@ e = 2.7182818285
 
 
 class Poisson:
-    """
-    Poisson class represents a Poisson distribution.
-    """
     def __init__(self, data=None, lambtha=1.):
-        if data is None:
+        if data is not None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(sum(data) / len(data))
+        else:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-        else:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
 
+    def pmf(self, k):
+        # Convert k to an integer
+        k = int(k)
 
-if __name__ == "__main__":
+        # Check if k is out of range (negative or not an integer)
+        if k < 0 or not isinstance(k, int):
+            return 0
 
-    data = [4, 3, 6, 8, 2]
-    p1 = Poisson(data)
-    print('Lambtha:', p1.lambtha)
-
-    p2 = Poisson(lambtha=5)
-    print('Lambtha:', p2.lambtha)
+        # Calculate the Poisson PMF value for k
+        import math
+        pmf_value = (self.lambtha ** k) * (math.exp(-self.lambtha) / math.factorial(k))
+        return pmf_value
