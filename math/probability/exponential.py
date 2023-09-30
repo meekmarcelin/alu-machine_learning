d63818dc6f98b8b7227768fc2ce144fa6e55217c
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-09-30 16:17:23 +0200

    5 commit

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,37 +1,24 @@
-#!/usr/bin/env python3
-"""Exponential distribution class."""
-
-π = 3.1415926536
-e = 2.7182818285
-
-
-class Exponential:
+def __init__(self, data=None, lambtha=1.):
     """
-    Class to represent an exponential distribution.
+    Initialize an Exponential distribution.
 
-    Attributes:
-        lambtha (float): Expected number of occurrences in a given time frame.
+    Args:
+        data (list): List of data to estimate the distribution.
+        lambtha (float): Expected number of occurrences.
+
+    Raises:
+        ValueError: If lambtha is not a positive value.
+        TypeError: If data is not a list or does not contain at least two data points.
     """
-
-    def __init__(self, data=None, lambtha=1.):
-        """
-        Initialize an Exponential distribution.
-
-        Args:
-            data (list): List of data to estimate the distribution.
-            lambtha (float): Expected number of occurrences.
-
-        Raises:
-            ValueError: If lambtha is not a positive value.
-            TypeError: If data is not a list or does not contain at least two data points.
-        """
-        if data is not None:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.lambtha = 1 / (sum(data) / len(data))
-        else:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+    if data is not None:
+        if not isinstance(data, list):
+            raise TypeError("data must be a list")
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        self.lambtha = 1 / (
+            sum(data) / len(data)
+        )
+    else:
+        if lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        self.lambtha = float(lambtha)
