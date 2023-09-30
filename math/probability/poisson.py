e2966ef0ade0223cb3e8cca581e3ddb83a3faa8b
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-09-30 08:58:46 +0200

    push

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 
 # Mathematical Approximations
-from poisson import Poisson
 π = 3.1415926536
 e = 2.7182818285
 
 class Poisson:
     """
     Poisson class represents a Poisson distribution.
-    
-    Class constructor __init__(self, data=None, lambtha=1.):
-    - data is a list of the data to be used to estimate the distribution.
-    - lambtha is the expected number of occurrences in a given time frame.
-    - Sets the instance attribute lambtha.
-    - Saves lambtha as a float.
-    - If data is not given (i.e., None), it uses the given lambtha.
-    - If lambtha is not a positive value or equals to 0, it raises a ValueError with the message lambtha must be a positive value.
-    - If data is given:
-      - Calculates the lambtha of data.
-      - If data is not a list, it raises a TypeError with the message data must be a list.
-      - If data does not contain at least two data points, it raises a ValueError with the message data must contain multiple values.
     """
 
     def __init__(self, data=None, lambtha=1.):
@@ -35,9 +22,8 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
 if __name__ == "__main__":
-
-    np.random.seed(0)
-    data = np.random.poisson(5., 100).tolist()
+    # Sample code
+    data = [4, 3, 6, 8, 2]
     p1 = Poisson(data)
     print('Lambtha:', p1.lambtha)
 
