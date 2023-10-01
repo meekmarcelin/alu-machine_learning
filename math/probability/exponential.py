54ce821da676bf30717e8f406dd57adda91c1c98
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-10-01 02:50:26 +0200

    commit

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
-"""Exponential distribution class."""
+"""Exponential distribution"""
 
 
-def __init__(self, data=None, lambtha=1.):
-    """
-    Initialize an Exponential distribution.
+class Exponential:
+    """Exponential distribution"""
 
-    Args:
-        data (list): List of data to estimate the distribution.
-        lambtha (float): Expected number of occurrences.
+    def __init__(self, data=None, lambtha=1.):
+        """Initialization"""
+        self.E = 2.7182818285
+        self.PI = 3.1415926536
 
-    Raises:
-        ValueError: If lambtha is not a positive value.
-        TypeError: If data is not a list or does not contain at least two data points.
-    """
-    if data is not None:
-        if not isinstance(data, list):
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
-        self.lambtha = 1 / (
-            sum(data) / len(data)
-        )
-    else:
-        if lambtha <= 0:
-            raise ValueError("lambtha must be a positive value")
-        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 1/(float(sum(data) / len(data)))
+
+    def pdf(self, x):
+        """ Calculates the value of the PDF for a given time period
+        """
+        if x < 0:
+            return 0
+        return self.lambtha * (self.E ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """ Calculates the value of the CDF for a given time period
+        """
+        if x < 0:
+            return 0
+        return 1 - (self.E ** (-self.lambtha * x))
