e5fca1aefe5e4ad55ff90ff5e644b92eaa671686
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-10-01 02:47:33 +0200

    push

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python3
+"""Poisson distribution"""
 
 
 class Poisson:
+    """POisson distribution class"""
+
     def __init__(self, data=None, lambtha=1.):
+        """Init"""
+        self.E = 2.7182818285
+        self.PI = 3.1415926536
+
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+            else:
+                self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = self.calculate_lambtha(data)
+            self.lambtha = float(sum(data) / len(data))
 
-    def calculate_lambtha(self, data):
-        total = sum(data)
-        return float(total) / len(data)
+    def factorial(self, k):
+        """ Find factorial of a number """
+        result = 1
+        for i in range(1, k+1):
+            result *= i
+        return result
 
     def pmf(self, k):
-        k = int(k)
+        """ Calculates the value of the PMF for a given number of "successes."
+
+        Args:
+            k (int): number of "successes"
+        """
+        if not isinstance(k, int):
+            k = int(k)
         if k < 0:
             return 0
-        else:
-            # Calculate the PMF using the Poisson distribution formula
-            pmf_value = (self.lambtha ** k) * (2.71828 **
-                                               (-self.lambtha)) / self.factorial(k)
-            return pmf_value
 
-    def factorial(self, n):
-        if n == 0 or n == 1:
-            return 1
-        else:
-            return n * self.factorial(n - 1)
+        p = (self.lambtha ** k) * (self.E ** -self.lambtha) / self.factorial(k)
+        return p
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of "successes."
+
+        Args:
+            k (int): is the number of successes.
+        """
+
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+
+        def p(k):
+
+            p = (self.lambtha ** k) * (self.E ** -
+                                       self.lambtha) / self.factorial(k)
+            return p
+        return sum([p(k) for k in range(0, k+1)])
