a03e1dd22dc7ea9d42cbe09aa64bc8c1e1f988c9
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2023-10-15 03:48:51 +0200

    commit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
+"""Function that calculates the determinant of a matrix"""
+
 
 def determinant(matrix):
-    """
-    Calculate the determinant of a square matrix
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    """Determinant calculation"""
+    # Handle the special case of 0x0 matrix
+    if matrix == [[]]:
+        return 1
+
+    # Check if matrix is a list of lists
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
-    # Check if the matrix is square
-    num_rows = len(matrix)
-    if num_rows == 0:
-        return 1.0  # A 0x0 matrix is considered to have a determinant of 1
-    if not all(len(row) == num_rows for row in matrix):
+    # Check if matrix is square
+    n = len(matrix)
+    if not all(len(row) == n for row in matrix):
         raise ValueError("matrix must be a square matrix")
 
-    # Base case for 1x1 matrix
-    if num_rows == 1:
-        return float(matrix[0][0])
+    # Base case: 1x1 matrix
+    if n == 1:
+        return matrix[0][0]
 
-    # Base case for 2x2 matrix
-    if num_rows == 2:
-        return float(matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0])
+    # Base case: 2x2 matrix
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
-    det = 0.0
-    for j in range(num_rows):
-        # Calculate the cofactor matrix
-        cofactor_matrix = [[matrix[i][k] for k in range(num_rows) if k != j]
-                          for i in range(1, num_rows)]
-        # Add the determinant of the current element multiplied by the cofactor determinant
-        det += float(matrix[0][j] * ((-1) ** j) * determinant(cofactor_matrix))
+    # Recursively calculate determinant for larger matrices
+    det = 0
+    for i in range(n):
+        # Create submatrix excluding the current row and column
+        submatrix = [row[:i] + row[i+1:] for row in matrix[1:]]
+        # Use the Laplace expansion to calculate determinant
+        det += matrix[0][i] * determinant(submatrix) * (-1 if i % 2 else 1)
 
     return det
