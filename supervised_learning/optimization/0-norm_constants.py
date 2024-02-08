80dd3523f45c91835584a64a5f826125ce60b2f0
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-02-08 04:03:29 +0200

    commit

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" constant normalization """
+
+
+def normalize_constants(x):
+    """ Return mena and standard deviation """
+ mean = np.mean(X, axis=0)
+ std_dev = np.std(X, axis=0)
+
+ return mean, std_dev   
