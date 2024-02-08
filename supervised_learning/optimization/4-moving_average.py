33957ac0f6ed5ce6e64c16beb1f557a86d3b2d0e
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-02-08 19:51:55 +0200

    commit

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 """Moving average"""
 
+
 def moving_average(data, beta):
-    """
-    Calculates the weighted moving average of a dataset.
+    """Calculates the weighted moving average of a data set"""
+    weighted_data = list()
+    epsilon = 1 - beta
+    V = 0
 
-    Args:
-    data: List of data points to calculate the moving average of.
-    beta: Weight used for the moving average.
+    for i, theta in enumerate(data, start=1):
+        bias_correction = 1 - (beta ** i)
+        V = (beta * V) + (epsilon * theta)
+        weighted_data.append(V / bias_correction)
 
-    Returns:
-    List containing the moving averages of the input data.
-    """
-    # Initialize variables
-    moving_avg = []
-    v_hat = 0
-    
-    # Calculate moving averages
-    for i in range(len(data)):
-        v_hat = beta * v_hat + (1 - beta) * data[i]
-        bias_correction = 1 - beta ** (i + 1)
-        moving_avg.append(v_hat / bias_correction)
-    
-    return moving_avg
+    return weighted_data
