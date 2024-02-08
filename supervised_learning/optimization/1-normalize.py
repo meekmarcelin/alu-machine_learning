24e42ff7f7b2e281c0f5d93239efa602d1abace6
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-02-08 04:38:26 +0200

    commit

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -5,8 +5,6 @@ import numpy as np
 
 def normalize(X, m, s):
     """Returns mean and standard deviation of input data"""
-    m = m.reshape((-1, 1))
-    s = s.reshape((-1, 1))
     X -= m
     X /= s
     return X
