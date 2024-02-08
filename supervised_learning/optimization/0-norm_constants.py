3ce753f52f655ff80174d6d62914cdabcd2fe145
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-02-08 04:26:00 +0200

    commit

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -5,6 +5,11 @@ import numpy as np
 
 def normalize_constants(X):
     """ Return mean and standard deviation """
-    mean = np.mean(X, axis=0)
-    std_dev = np.std(X, axis=0)
-    return mean, std_dev
+     X_ref = X.copy()
+    m, nx = X_ref.shape
+    mean = (1 / m) * X_ref.sum(axis=0)
+    X_ref -= mean
+    X_ref = X_ref ** 2
+    var = (1 / m) * X_ref.sum(axis=0)
+    dv = np.sqrt(var)
+    return mean, dv
