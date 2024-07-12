bbc90b52d9193e052cfd221e9400b1dc2ded4227
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-07-12 02:57:14 +0200

    commit

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -8,12 +8,14 @@ noiseless 1D Gaussian process
 import numpy as np
 GP = __import__('2-gp').GaussianProcess
 
+
 class BayesianOptimization:
     """
     A class that represents a Bayesian optimization on a
     noiseless 1D Gaussian process
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         Initializes Bayesian Optimization
         """
